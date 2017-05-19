ec143cdcf5fdab376cbd353cbe581f4c09d7baa3
Ivan <Skayline07@mail.ru>
2017-05-19 15:46:27 +0300

    Added library mouse scroll

--- src/js/eventHandler.js
+++ src/js/eventHandler.js
@@ -26,35 +26,17 @@ function clickArrow(direction) {
     }
 }
 
-function onWheel(e) {
-    e = e || window.event;
-    // wheelDelta не дает возможность узнать количество пикселей
-    var delta = e.deltaY || e.detail || e.wheelDelta;
-
-    if (delta > 119) {
-        clickArrow('bottom');
-    }
-    else if (delta < -119) {
-        clickArrow('top');
-    }
-
-    e.preventDefault ? e.preventDefault() : (e.returnValue = false);
-}
-
 function initScrollHandler() {
-    if (document.body.addEventListener) {
-        if ('onwheel' in document) {
-            // IE9+, FF17+, Ch31+
-            document.body.addEventListener("wheel", onWheel);
-        } else if ('onmousewheel' in document) {
-            // устаревший вариант события
-            document.body.addEventListener("mousewheel", onWheel);
-        } else {
-            // Firefox < 17
-            document.body.addEventListener("MozMousePixelScroll", onWheel);
-        }
-    } else { // IE8-
-        document.body.attachEvent("onmousewheel", onWheel);
-    }
+    var indicator = new WheelIndicator({
+        callback: function(e){
+            if (e.direction == 'down') {
+                clickArrow('bottom');
+            }
+            else if (e.direction == 'up') {
+                clickArrow('top');
+            }
+        },
+        preventMouse: false
+    });
 
 }
