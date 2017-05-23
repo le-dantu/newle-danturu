ba3a8c6e6aa557b8fd10913426cfea094b470e8a
Ivan <Skayline07@mail.ru>
2017-05-23 15:09:29 +0300

    fix double js folder

--- src/js/reciveData.js
+++ src/js/reciveData.js
@@ -5,10 +5,16 @@
 function sendRecive(link) {
     var links = [];
     $.getJSON(link, function(data) {
-        $.each(data.type.company, function(key, value) {
-            links.push(value);
+        $.each(data, function(key, value) {
+            $.each(data[key], function(key1, value1) {
+                $.each(data[key][key1], function(key2, value2) {
+                    $.each(data[key][key1][key2], function(key3, value3) {
+                        links.push(value3);
+                    });
+                });
+            });
         });
-    });
+     });
 
     return links;
 }
