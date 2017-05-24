161483791294db83997226d0719bf61ab0cd4e94
SoNWarrioR <azot18@gmail.com>
2017-05-24 14:54:08 +0300

    remove git changes

--- www/js/variable.js
+++ www/js/variable.js
@@ -2,17 +2,14 @@
  * Created by BreakPoint on 19.05.17.
  */
 
-<<<<<<< HEAD
 var gateway = '../modules/content.json';
 var links = [];
-=======
 var contentGlobal = '/content.json';
 //var clientsGlobal = '/clients.json';
 //var workTypeGlobal = '/worktype.json';
 //var contentTypeGlobal = '/contenttype.json';
 var horizontal = [];
 var vertical = [];
->>>>>>> Break
 var lenCurrent = 0;
 var currentImg = 0;
 var currentType = 0;
