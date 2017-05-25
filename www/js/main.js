c209f3a0b9c939fbb718f1d0d4d860ac6709b382
SoNWarrioR <azot18@gmail.com>
2017-05-25 18:44:23 +0300

    create url sef

--- www/js/main.js
+++ www/js/main.js
@@ -1,7 +1,21 @@
 /**
  * Created by BreakPoint on 18.05.17.
  */
-
+function cleanArray(actual) {
+  var newArray = new Array();
+  for (var i = 0; i < actual.length; i++) {
+    if (actual[i]) {
+      newArray.push(actual[i]);
+    }
+  }
+  return newArray;
+}
+var locationurl = window.location.pathname.split("/");
+locationurl = cleanArray(locationurl);
+if (locationurl.length == 1) {
+    locationurl[1] = locationurl[0];
+    locationurl[2] = locationurl[0];
+}
 $(document).ready(function(){
     $('button.arrow').each(function(){
         if ( $(this).parent().hasClass('navLR')) {
