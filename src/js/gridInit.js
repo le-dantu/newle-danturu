3d1e0e64edc3da5724992e4fb3b3dfa3262e243b
SoNWarrioR <azot18@gmail.com>
2017-06-06 15:16:46 +0300

    #fix

--- src/js/gridInit.js
+++ src/js/gridInit.js
@@ -4,12 +4,22 @@ function gridInit() {
 		re = /style=\'.*\)\'/ig
 		ig = /\(.*\)/ig
 		//console.log(contentGlobal[$(this).data('type')][first(contentGlobal[$(this).data('type')])][0]);
-		var ad = contentGlobal['portfolio'][$(this).data('type')][first(contentGlobal['portfolio'][$(this).data('type')])][0].match(ig);
-		ad = ad[0].replace(/\(/,"");
-		ad = ad.replace(/lg/,"sm");
-		ad = ad.replace(/lg\.png/,"sm\.jpg");
-		$(this).attr("style", "background: url("+ad);
-		$(this).attr('onclick', "window.location.href = '/portfolio/" + first(contentGlobal['portfolio'][$(this).data('type')]) + "/" + $(this).data('type') + "/'");
+		if ($(this).data('key')) {
+			var ad = contentGlobal['portfolio'][$(this).data('type')][$(this).data('client')][$(this).data('key')].match(ig);
+			ad = ad[0].replace(/\(/,"");
+			ad = ad.replace(/lg/,"sm");
+			ad = ad.replace(/lg/,"sm");
+			ad = ad.replace(/png/,"jpg");
+			$(this).attr("style", "background: url("+ad);
+			$(this).attr('onclick', "window.location.href = '/portfolio/" + $(this).data('client') + "/" + $(this).data('type') + "/" + $(this).data('key') +"'");
+		} else {
+			var ad = contentGlobal['portfolio'][$(this).data('type')][first(contentGlobal['portfolio'][$(this).data('type')])][0].match(ig);
+			ad = ad[0].replace(/\(/,"");
+			ad = ad.replace(/lg/,"sm");
+			ad = ad.replace(/lg\.png/,"sm\.jpg");
+			$(this).attr("style", "background: url("+ad);
+			$(this).attr('onclick', "window.location.href = '/portfolio/" + first(contentGlobal['portfolio'][$(this).data('type')]) + "/" + $(this).data('type') + "/'");
+		}
 	});
     $('.navLR').hide();
     $('.navTB').hide();
