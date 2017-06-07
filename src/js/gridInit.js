ad49974395860a7568264af72c3eed11b2d0ae09
SoNWarrioR <azot18@gmail.com>
2017-06-07 18:54:58 +0300

    #1481

--- src/js/gridInit.js
+++ src/js/gridInit.js
@@ -6,20 +6,14 @@ function gridInit() {
 		//console.log(contentGlobal[$(this).data('type')][first(contentGlobal[$(this).data('type')])][0]);
 		if ($(this).data('key') !== undefined) {
 			var ad = contentGlobal['portfolio'][$(this).data('type')][$(this).data('client')][$(this).data('key')].match(ig);
-			ad = ad[0].replace(/\(/,"");
-			ad = ad.replace(/lg/,"sm");
-			ad = ad.replace(/lg/,"sm");
-			ad = ad.replace(/png/,"jpg");
+			ad = ad[0].replace(/\(/,"").replace(/lg/,"sm").replace(/lg/,"sm").replace(/png/,"jpg");
 			$(this).attr("style", "background: url("+ad);
-			$(this).attr('onclick', "window.location.href = '/portfolio/" + $(this).data('client') + "/" + $(this).data('type') + "/" + $(this).data('key') +"'");
+			$(this).attr('onclick', "initFirstBlock('portfolio', '" + $(this).data('type') + "', '" + $(this).data('client') + "', '" + $(this).data('key') + "')");
 		} else {
 			var ad = contentGlobal['portfolio'][$(this).data('type')][first(contentGlobal['portfolio'][$(this).data('type')])][first(contentGlobal['portfolio'][$(this).data('type')][first(contentGlobal['portfolio'][$(this).data('type')])])].match(ig);
-			ad = ad[0].replace(/\(/,"");
-			ad = ad.replace(/lg/,"sm");
-			ad = ad.replace(/lg/,"sm");
-			ad = ad.replace(/png/,"jpg");
+			ad = ad[0].replace(/\(/,"").replace(/lg/,"sm").replace(/lg/,"sm").replace(/png/,"jpg");
 			$(this).attr("style", "background: url("+ad);
-			$(this).attr('onclick', "window.location.href = '/portfolio/" + first(contentGlobal['portfolio'][$(this).data('type')]) + "/" + $(this).data('type') + "/'");
+			$(this).attr('onclick', "initFirstBlock('portfolio', '" + $(this).data('type') + "', '" + first(contentGlobal['portfolio'][$(this).data('type')]) + "')");
 		}
 	});
     $('.navLR').hide();
