0895e6723ff7975b77948ca4303f9506e5964700
SoNWarrioR <azot18@gmail.com>
2017-06-13 16:20:36 +0300

    edit style and menu pug

--- src/js/gridInit.js
+++ src/js/gridInit.js
@@ -4,12 +4,14 @@ function gridInit() {
 		re = /style=\'.*\)\'/ig
 		ig = /\(.*\)/ig
 		//console.log(contentGlobal[$(this).data('type')][first(contentGlobal[$(this).data('type')])][0]);
-		if ($(this).data('key') !== undefined) {
+		if ($(this).data('key') !== "undefined") {
+
 			var ad = contentGlobal['portfolio'][$(this).data('type')][$(this).data('client')][$(this).data('key')].match(ig);
 			ad = ad[0].replace(/\(/,"").replace(/lg/,"sm").replace(/lg/,"sm").replace(/png/,"jpg");
 			$(this).attr("style", "background: url("+ad);
 			$(this).attr('onclick', "initFirstBlock('portfolio', '" + $(this).data('type') + "', '" + $(this).data('client') + "', '" + $(this).data('key') + "')");
 		} else {
+
 			var ad = contentGlobal['portfolio'][$(this).data('type')][first(contentGlobal['portfolio'][$(this).data('type')])][first(contentGlobal['portfolio'][$(this).data('type')][first(contentGlobal['portfolio'][$(this).data('type')])])].match(ig);
 			ad = ad[0].replace(/\(/,"").replace(/lg/,"sm").replace(/lg/,"sm").replace(/png/,"jpg");
 			$(this).attr("style", "background: url("+ad);
